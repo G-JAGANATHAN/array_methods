9ab2993bc697f7e1cc8488c4c3849d26393aab0a
jaganathantnea@gmail.com <jaganathan@JAGANATHANs-MacBook-Air.local>
2026-08-26 10:59:38 +0530

    first commit

---
+++ length.js
@@ -0,0 +1,28 @@
+let fruits=["mango","apple","banana","orange"];
+console.log(fruits.length);//output=4
+
+//Numbers length
+let numbers=[18,26,23,53,75,82,36];
+console.log(numbers.length)//output=7
+
+//subjects length 
+let subjects=["Java", "Python", "HTML", "CSS", "JavaScript"];
+console.log(subjects.length);//output=5
+
+//Empty array
+let array=[];
+console.log(array.length);//output=0
+
+//using lenghth in a loop 
+let cars=["BMW", "Audi", "Tesla","Tata"];
+for(i=0;i<cars.length;i++){
+ console.log(cars[i]);}
+//output:
+//BMW
+//Audi
+//Tesla
+//Tata
+
+//animals
+let animals=["Dog", "Cat", "Lion", "Tiger", "Elephant"];
+console.log(animals.length);//output=5
