fa35797f731374ca70c33fd63ca4227a9b1a6e10
jaganathantnea@gmail.com <jaganathan@JAGANATHANs-MacBook-Air.local>
2026-08-26 10:34:17 +0530

    first commit

---
+++ array_join.js
@@ -0,0 +1,23 @@
+// basic join
+let fruits=["apple","mango","orange","banana","berry"]
+console.log(fruits.join());//ouyput=apple,mango,orange,banana,berry
+
+//jion with a space
+console.log(fruits.join(" "));//output=apple mango orange banana berry
+
+//join with -
+{let numbers=[1,2,3,4,5]
+console.log(numbers.join("-"));//output=1-2-3-4-5
+
+//join with comma and space
+console.log(numbers.join(", "))//output=1, 2, 3, 4, 5
+}
+
+//join with |
+let countries=["india","japan","canada","australia"]
+console.log(countries.join("|")); //output=india|japan|canada|australia
+
+//join with + use subject variable 
+let subject=["maths","physics","java","Dbms"]
+console.log(subjects.join("+"));//output=maths+physics+java+Dbms
+
